1b91726463a59fc6cd9951089640efcae3f4d7e5
Unknown <bearps@microsoft.com>
2019-02-18 15:15:34 +1300

    Implement VocabularySet.Add() and test

---
+++ src/WordTutor.Core/VocabularySet.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Text;
+
+namespace WordTutor.Core
+{
+    /// <summary>
+    /// A set of words that make up a vocabulary for spelling drills
+    /// </summary>
+    public class VocabularySet
+    {
+        public static VocabularySet Empty = new VocabularySet();
+
+        public ImmutableDictionary<string, VocabularyWord> Words { get; }
+
+        public VocabularySet Add(VocabularyWord word)
+        {
+            if (word == null)
+            {
+                throw new ArgumentNullException(nameof(word));
+            }
+
+            if (Words.ContainsKey(word.Spelling))
+            {
+                throw new ArgumentException(
+                    $"A word with spelling {word.Spelling} already exists in this set.",
+                    nameof(word));
+            }
+
+            var words = Words.Add(word.Spelling, word);
+            return new VocabularySet(this, words: words);
+        }
+
+        private VocabularySet()
+        {
+            Words = ImmutableDictionary<string, VocabularyWord>.Empty;
+        }
+
+        private VocabularySet(
+            VocabularySet original,
+            ImmutableDictionary<string,VocabularyWord> words = null)
+        {
+            Words = words ?? original.Words;
+        }
+    }
+
+}
---
+++ tests/WordTutor.Core.Tests/VocabularySetTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace WordTutor.Core.Tests
+{
+    public class VocabularySetTests
+    {
+        public class Empty : VocabularySetTests
+        {
+            [Fact]
+            public void ReturnsSetWithNoWords()
+            {
+                VocabularySet.Empty.Words.Should().BeEmpty();
+            }
+        }
+
+        public class Add : VocabularySetTests
+        {
+            private VocabularySet _empty = VocabularySet.Empty;
+
+            [Fact]
+            public void GivenNull_ThrowsException()
+            {
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _empty.Add(null));
+                exception.ParamName.Should().Be("word");
+            }
+
+            [Fact]
+            public void GivenNewWord_ReturnsSetContainingWord()
+            {
+                var word = new VocabularyWord("bumble");
+                var set = _empty.Add(word);
+                set.Words.Should().ContainValue(word);
+            }
+
+            [Fact]
+            public void GivenExistingWord_ThrowsException()
+            {
+                var word = new VocabularyWord("bumble");
+                var set = _empty.Add(word);
+                var exception = Assert.Throws<ArgumentException>(
+                    () => set.Add(word));
+                exception.ParamName.Should().Be("word");
+                exception.Message.Should().Contain("already exists");
+
+            }
+        }
+    }
+}
