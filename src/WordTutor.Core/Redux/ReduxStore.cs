66e2305186b8c3754b55eb7ac0cf75d4ca90980d
Unknown <bearps@microsoft.com>
2019-03-17 20:05:56 +1300

    Create ReduxStore and test
    Includes supporting classes

---
+++ src/WordTutor.Core/Redux/ReduxStore.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WordTutor.Core.Redux
+{
+    /// <summary>
+    /// Central store for the state of the application
+    /// </summary>
+    public class ReduxStore<T>
+    {
+        // Reference to our state reducer
+        private readonly IReduxReducer<T> _reducer;
+
+        /// <summary>
+        /// Gets the current state of the application
+        /// </summary>
+        public T State { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ReduxStore{T}" class/>
+        /// </summary>
+        /// <param name="reducer">Reducer to use for state transformations.</param>
+        /// <param name="initialState">Initial state for the application.</param>
+        public ReduxStore(IReduxReducer<T> reducer, T initialState)
+        {
+            _reducer = reducer ?? throw new ArgumentNullException(nameof(reducer));
+            State = initialState;
+        }
+
+        /// <summary>
+        /// Dispatch an application message to modify application state
+        /// </summary>
+        /// <param name="message">Message to process.</param>
+        public void Dispatch(IReduxMessage message)
+        {
+            State = _reducer.Reduce(
+                message ?? throw new ArgumentNullException(nameof(message)), 
+                State);
+        }
+    }
+}
---
+++ tests/WordTutor.Core.Tests/Fakes/FakeReducer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using WordTutor.Core.Redux;
+
+namespace WordTutor.Core.Tests.Fakes
+{
+    public class FakeReducer<T> : IReduxReducer<T>
+    {
+        public Func<IReduxMessage, T, T> Reduce { get; set; } = (m, s) => s;
+
+        T IReduxReducer<T>.Reduce(IReduxMessage message, T currentState) 
+            => Reduce(message, currentState);
+    }
+}
