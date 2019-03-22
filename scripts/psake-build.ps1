b7c17026efeb77c400c29e525a2239b67b1a70d5
Unknown <bearps@microsoft.com>
2019-03-23 11:40:53 +1300

    Integrate ReportGenerator

--- scripts/psake-build.ps1
+++ scripts/psake-build.ps1
@@ -10,7 +10,7 @@ properties {
 }
 
 # Do our integration build 
-Task Integration.Build -Depends Clean.SourceFolder, Unit.Tests
+Task Integration.Build -Depends Clean.BuildDir, Clean.SourceDir, Compile, Unit.Tests, Coverage.Report
 
 ## --------------------------------------------------------------------------------
 ##   Prerequisite Targets
@@ -31,12 +31,49 @@ Task Requires.DotNetExe {
     Write-Output "Dotnet executable: $dotnetExe"
 }
 
+Task Requires.ReportGenerator -Depends Requires.DotNetExe {
+
+    $toNatural = { [regex]::Replace($_, '\d+', { $args[0].Value.PadLeft(20) }) }
+
+    # dotnet returns a list of the form "info : label : folder"
+    # So we strip "info : " from the start of each line
+    Write-Output "[-] Finding NuGet caches via dotnet.exe"
+    $locals = & $dotnetExe nuget locals all --list | % { $_.SubString(7) }
+
+    foreach($local in $locals)
+    {
+        $index = $local.IndexOf(":")
+        $folder = $local.Substring($index + 1).Trim()
+
+        $exePath = `
+            get-childitem $folder\reportgenerator\*\ReportGenerator.exe `
+                -ErrorAction SilentlyContinue -Recurse `
+            | Sort-Object $toNatural | select-object -last 1
+
+        if ($exePath -ne $null)
+        {
+            Write-Output "[+] ReportGenerator found at $exePath"
+            $script:reportGeneratorExe = $exePath
+            break
+        }
+        else 
+        {
+            Write-Output "[x] ReportGenerator not found within $folder"
+        }
+    }
+
+    if ($reportGeneratorExe -eq $null)
+    {
+        throw "Failed to find ReportGenerator.exe"
+    }
+}
+
 ## --------------------------------------------------------------------------------
 ##   Cleaning Targets
 ## --------------------------------------------------------------------------------
 ## Tasks used to clean up 
 
-Task Clean.SourceFolder {
+Task Clean.SourceDir {
 
     Write-Info "Cleaning $srcDir"
     remove-item $srcDir\*\bin\* -recurse -ErrorAction SilentlyContinue
@@ -47,6 +84,31 @@ Task Clean.SourceFolder {
     remove-item $testsDir\*\bin\* -recurse -ErrorAction SilentlyContinue
     remove-item $testsDir\*\obj\* -recurse -ErrorAction SilentlyContinue
     remove-item $testsDir\*\publish\* -recurse -ErrorAction SilentlyContinue
+}
+
+Task Clean.BuildDir {
+
+    $script:buildDir = join-path $baseDir "build"
+
+    Write-Host "Build output folder: $buildDir"
+
+    if (test-path $buildDir) {
+        remove-item $buildDir -recurse -force -ErrorAction SilentlyContinue
+    }
+
+    mkdir $buildDir -ErrorAction SilentlyContinue | Out-Null
+}
+
+Task Clean.CoverageReportDir -Depends Clean.BuildDir {
+
+    $script:coverageReportDir = join-path $buildDir coverage
+    Write-Host "Coverage report folder: $coverageReportDir"
+
+    if (test-path $coverageReportDir) {
+        remove-item $coverageReportDir -recurse -force -ErrorAction SilentlyContinue
+    }
+
+    mkdir $coverageReportDir -ErrorAction SilentlyContinue | Out-Null
 }
 
 ## --------------------------------------------------------------------------------
@@ -69,9 +131,19 @@ Task Unit.Tests -Depends Requires.DotNetExe, Compile {
         $projectName = (get-item $project).BaseName
         Write-SubtaskName $projectName
         exec {
-            & $dotnetExe test $project /p:CollectCoverage=true /p:Exclude="[xunit*]*%2c[*.Tests]*"
+            & $dotnetExe test $project /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[xunit*]*%2c[*.Tests]*"
         }
     }
+}
+
+Task Coverage.Report -Depends Requires.ReportGenerator, Clean.CoverageReportDir, Unit.Tests {
+
+    exec {
+        & $reportGeneratorExe -reports:$testsDir\*\*.opencover.xml -targetdir:$coverageReportDir
+    }
+
+    $openCoverIndex = resolve-path $coverageReportDir\index.htm
+    & $openCoverIndex
 }
 
 ## --------------------------------------------------------------------------------
