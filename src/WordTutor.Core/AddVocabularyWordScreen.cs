a66bacb3b774e4f46f83ca17e28b3f87d19dae24
Unknown <bearps@microsoft.com>
2019-04-09 21:37:10 +1200

    Create AddVocabularyWordScreen and test

---
+++ src/WordTutor.Core/AddVocabularyWordScreen.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordTutor.Core
+{
+    public class AddVocabularyWordScreen : Screen
+    {
+        /// <summary>
+        /// Gets the spelling currently shown on screen
+        /// </summary>
+        public string Spelling { get; } = string.Empty;
+
+        /// <summary>
+        /// Gets the pronunciation currently shown on screen
+        /// </summary>
+        public string Pronunciation { get; } = string.Empty;
+
+        /// <summary>
+        /// Gets the guide phrase currently shown on screen
+        /// </summary>
+        public string Phrase { get; } = string.Empty;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AddVocabularyWordScreen"/> class
+        /// </summary>
+        public AddVocabularyWordScreen()
+        {
+        }
+
+        public AddVocabularyWordScreen WithSpelling(string spelling)
+        {
+            return new AddVocabularyWordScreen(
+                this,
+                spelling: spelling ?? string.Empty);
+        }
+
+        public AddVocabularyWordScreen WithPhrase(string phrase)
+        {
+            return new AddVocabularyWordScreen(
+                this,
+                phrase: phrase ?? string.Empty);
+        }
+
+        public AddVocabularyWordScreen WithPronunciation(string pronunciation)
+        {
+            return new AddVocabularyWordScreen(
+                this,
+                pronunciation: pronunciation ?? string.Empty);
+        }
+
+        protected AddVocabularyWordScreen(
+            AddVocabularyWordScreen original,
+            string spelling = null,
+            string pronunciation = null,
+            string phrase = null)
+        {
+            if (original is null)
+            {
+                throw new ArgumentNullException(nameof(original));
+            }
+
+            Spelling = spelling ?? original.Spelling;
+            Pronunciation = pronunciation ?? original.Pronunciation;
+            Phrase = phrase ?? original.Phrase;
+        }
+    }
+}
---
+++ tests/WordTutor.Core.Tests/AddVocabularyWordScreenTests.cs
@@ -0,0 +1,166 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace WordTutor.Core.Tests
+{
+    public class AddVocabularyWordScreenTests
+    {
+        public class Constructor : AddVocabularyWordScreenTests
+        {
+            private readonly AddVocabularyWordScreen _screen = new AddVocabularyWordScreen();
+
+            [Fact]
+            public void CreatesScreenWithNoSpelling()
+            {
+                _screen.Spelling.Should().Be(string.Empty);
+            }
+
+            [Fact]
+            public void CreatesScreenWithNoPhrase()
+            {
+                _screen.Phrase.Should().Be(string.Empty);
+            }
+
+            [Fact]
+            public void CreatesScreenWithNoPronunciation()
+            {
+                _screen.Pronunciation.Should().Be(string.Empty);
+            }
+        }
+
+        public class WithSpelling : AddVocabularyWordScreenTests
+        {
+            private readonly AddVocabularyWordScreen _screen =
+                new AddVocabularyWordScreen()
+                    .WithSpelling("Foo")
+                    .WithPhrase("Do the Foo")
+                    .WithPronunciation("Fu");
+
+            [Fact]
+            public void GivenSpelling_UpdatesProperty()
+            {
+                var s = "Bar";
+                _screen.WithSpelling(s).Spelling.Should().Be(s);
+            }
+
+            [Fact]
+            public void GivenEmptySpelling_UpdatesProperty()
+            {
+                _screen.WithSpelling(string.Empty).Spelling
+                    .Should().Be(string.Empty);
+            }
+
+            [Fact]
+            public void GivenNullSpelling_UpdatesPropertyToEmpty()
+            {
+                _screen.WithSpelling(null).Spelling
+                    .Should().Be(string.Empty);
+            }
+
+            [Fact]
+            public void GivenSpelling_PreservesPhrase()
+            {
+                _screen.WithSpelling("Baz").Phrase
+                    .Should().Be(_screen.Phrase);
+            }
+
+            [Fact]
+            public void GivenSpelling_PreservesPronunciation()
+            {
+                _screen.WithSpelling("Baz").Pronunciation
+                    .Should().Be(_screen.Pronunciation);
+            }
+        }
+
+        public class WithPhrase : AddVocabularyWordScreenTests
+        {
+            private readonly AddVocabularyWordScreen _screen =
+                new AddVocabularyWordScreen()
+                    .WithSpelling("Foo")
+                    .WithPhrase("Do the Foo")
+                    .WithPronunciation("Fu");
+
+            [Fact]
+            public void GivenPhrase_UpdatesProperty()
+            {
+                var s = "Do the Bar";
+                _screen.WithPhrase(s).Phrase.Should().Be(s);
+            }
+
+            [Fact]
+            public void GivenEmptyPhrase_UpdatesProperty()
+            {
+                _screen.WithPhrase(string.Empty)
+                    .Phrase.Should().Be(string.Empty);
+            }
+
+            [Fact]
+            public void GivenNullPhrase_UpdatesPropertyToEmpty()
+            {
+                _screen.WithPhrase(null)
+                    .Phrase.Should().Be(string.Empty);
+            }
+
+            [Fact]
+            public void GivenPhrase_PreservesSpelling()
+            {
+                _screen.WithPhrase("Do the Baz")
+                    .Spelling.Should().Be(_screen.Spelling);
+            }
+
+            [Fact]
+            public void GivenSpelling_PreservesPronunciation()
+            {
+                _screen.WithPhrase("Do the Baz")
+                    .Pronunciation.Should().Be(_screen.Pronunciation);
+            }
+        }
+
+        public class WithPronunciation : AddVocabularyWordScreenTests
+        {
+            private readonly AddVocabularyWordScreen _screen =
+                new AddVocabularyWordScreen()
+                    .WithSpelling("Foo")
+                    .WithPhrase("Do the Foo")
+                    .WithPronunciation("Fu");
+
+            [Fact]
+            public void GivenPronunciation_UpdatesProperty()
+            {
+                var s = "Bu";
+                _screen.WithPronunciation(s).Pronunciation.Should().Be(s);
+            }
+
+            [Fact]
+            public void GivenEmptyPronunciation_UpdatesProperty()
+            {
+                _screen.WithPronunciation(string.Empty)
+                    .Pronunciation.Should().Be(string.Empty);
+            }
+
+            [Fact]
+            public void GivenNullPronunciation_UpdatesPropertyToEmpty()
+            {
+                _screen.WithPronunciation(null)
+                    .Pronunciation.Should().Be(string.Empty);
+            }
+
+            [Fact]
+            public void GivenPronunciation_PreservesSpelling()
+            {
+                _screen.WithPronunciation("Bar")
+                    .Spelling.Should().Be(_screen.Spelling);
+            }
+
+            [Fact]
+            public void GivenPronunciation_PreservesPhrase()
+            {
+                _screen.WithPronunciation("Do the Baz")
+                    .Phrase.Should().Be(_screen.Phrase);
+            }
+        }
+    }
+}
