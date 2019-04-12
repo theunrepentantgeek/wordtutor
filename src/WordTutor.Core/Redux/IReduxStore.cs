b8027bbed8503edc9109a0ffacebcd48fb04d3b1
Unknown <bearps@microsoft.com>
2019-04-13 11:59:43 +1200

    Extract interface for ReduxStore to allow mocking

---
+++ src/WordTutor.Core/Redux/IReduxStore.cs
@@ -0,0 +1,16 @@
+﻿namespace WordTutor.Core.Redux
+{
+    public interface IReduxStore<T>
+    {
+        /// <summary>
+        /// Gets the current state of the application
+        /// </summary>
+        T State { get; }
+
+        /// <summary>
+        /// Dispatch an application message to modify application state
+        /// </summary>
+        /// <param name="message">Message to process.</param>
+        void Dispatch(IReduxMessage message);
+    }
+}
