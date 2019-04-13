58b63352d9cb1dc204d09e9faee0b79103969977
Unknown <bearps@microsoft.com>
2019-04-13 19:18:21 +1200

    Create FakeApplicationStore for testing

---
+++ tests/WordTutor.Desktop.Tests/Fakes/FakeApplicationStore.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WordTutor.Core;
+using WordTutor.Core.Redux;
+
+namespace WordTutor.Desktop.Tests.Fakes
+{
+    public class FakeApplicationStore : IReduxStore<WordTutorApplication>
+    {
+        public WordTutorApplication State { get; }
+
+        public IReduxMessage Message { get; private set; }
+
+        public FakeApplicationStore(WordTutorApplication application)
+        {
+            State = application;
+        }
+
+        public void Dispatch(IReduxMessage message) => Message = message;
+    }
+}
