d5e29403d64aec6319b6333b4ea44d2cc375d816
Unknown <bearps@microsoft.com>
2019-04-21 19:37:08 +1200

    Create VocabularyWordsBrowserScreen and test

---
+++ src/WordTutor.Core/VocabularyBrowserScreen.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace WordTutor.Core
+{
+    public class VocabularyBrowserScreen : Screen
+    {
+        /// <summary>
+        /// Gets the currently selected word
+        /// </summary>
+        public VocabularyWord Selection { get; }
+
+        /// <summary>
+        /// Gets a value indicating whether we have unsaved changes
+        /// </summary>
+        public bool Modified { get; }
+
+        public VocabularyBrowserScreen()
+        {
+        }
+
+        public VocabularyBrowserScreen WithSelection(VocabularyWord word)
+        {
+            if (Equals(Selection, word ?? throw new ArgumentNullException(nameof(word))))
+            {
+                return this;
+            }
+            return new VocabularyBrowserScreen(
+                this,
+                selection: word);
+        }
+
+        public VocabularyBrowserScreen MarkAsModified()
+        {
+            if (Modified)
+            {
+                return this;
+            }
+
+            return new VocabularyBrowserScreen(
+                this,
+                modified: true);
+        }
+
+        public VocabularyBrowserScreen MarkAsUnmodified()
+        {
+            if (!Modified)
+            {
+                return this;
+            }
+
+            return new VocabularyBrowserScreen(
+                this,
+                modified: false);
+        }
+
+        private VocabularyBrowserScreen(
+            VocabularyBrowserScreen original,
+            VocabularyWord selection = null,
+            bool? modified = null)
+        {
+            Selection = selection ?? original.Selection;
+            Modified = modified ?? original.Modified;
+        }
+    }
+}
---
+++ tests/WordTutor.Core.Tests/VocabularyBrowserScreenTests.cs
@@ -0,0 +1,99 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace WordTutor.Core.Tests
+{
+    public class VocabularyBrowserScreenTests
+    {
+        public class Constructor : VocabularyBrowserScreenTests
+        {
+            [Fact]
+            public void AfterConstruction_SelectionIsNull()
+            {
+                var browser = new VocabularyBrowserScreen();
+                browser.Selection.Should().BeNull();
+            }
+
+            [Fact]
+            public void AfterConstruction_ModifiedIsFalse()
+            {
+                var browser = new VocabularyBrowserScreen();
+                browser.Modified.Should().BeFalse();
+            }
+        }
+
+        public class WithSelection : VocabularyBrowserScreenTests
+        {
+            private readonly VocabularyBrowserScreen _browser;
+            private readonly VocabularyWord _foo = new VocabularyWord("foo");
+            private readonly VocabularyWord _bar = new VocabularyWord("bar");
+
+            public WithSelection()
+            {
+                _browser = new VocabularyBrowserScreen()
+                    .WithSelection(_foo);
+            }
+
+            [Fact]
+            public void GivenNull_ThrowsExpectedException()
+            {
+                var exception =
+                    Assert.Throws<ArgumentNullException>(
+                        () => _browser.WithSelection(null));
+                exception.ParamName.Should().Be("word");
+            }
+
+            [Fact]
+            public void GivenWord_ModifiesProperty()
+            {
+                var browser = _browser.WithSelection(_bar);
+                browser.Selection.Should().Be(_bar);
+            }
+
+            [Fact]
+            public void GivenExistingSelection_ReturnsSameInstance()
+            {
+                var browser = _browser.WithSelection(_browser.Selection);
+                browser.Should().BeSameAs(_browser);
+            }
+
+            [Theory]
+            [InlineData(true)]
+            [InlineData(false)]
+            public void ChangingSelection_DoesNotChangeModified(bool initiallyModified)
+            {
+                var initialBrowser = initiallyModified
+                    ? _browser.MarkAsModified()
+                    : _browser.MarkAsUnmodified();
+                var browser = initialBrowser.WithSelection(_bar);
+                browser.Modified.Should().Be(initialBrowser.Modified);
+            }
+        }
+
+        public class MarkAsModified : VocabularyBrowserScreenTests
+        {
+            private readonly VocabularyBrowserScreen _browser = new VocabularyBrowserScreen();
+
+            [Fact]
+            public void SetsPropertyModified()
+            {
+                var browser = _browser.MarkAsModified();
+                browser.Modified.Should().BeTrue();
+            }
+        }
+
+        public class MarkAsUnmodified : VocabularyBrowserScreenTests
+        {
+            private readonly VocabularyBrowserScreen _browser =
+                new VocabularyBrowserScreen().MarkAsModified();
+
+            [Fact]
+            public void ClearsPropertyModified()
+            {
+                var browser = _browser.MarkAsUnmodified();
+                browser.Modified.Should().BeFalse();
+            }
+        }
+    }
+}
