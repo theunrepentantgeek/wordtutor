576bbe3854bb8e71311a3c156865ab081416e7bd
Unknown <bearps@microsoft.com>
2019-07-06 15:30:48 +1200

    Create AddVocabularyWordScreenReducer and test

---
+++ src/WordTutor.Core/Reducers/AddVocabularyWordScreenReducer.cs
@@ -0,0 +1,33 @@
+﻿using WordTutor.Core.Actions;
+using WordTutor.Core.Redux;
+
+namespace WordTutor.Core.Reducers
+{
+    public class AddVocabularyWordScreenReducer : IReduxReducer<WordTutorApplication>
+    {
+        public WordTutorApplication Reduce(IReduxMessage message, WordTutorApplication currentState)
+        {
+            if (!(currentState.CurrentScreen is AddVocabularyWordScreen))
+            {
+                return currentState;
+            }
+
+            switch (message)
+            {
+                case ModifySpellingMessage m:
+                    return currentState.UpdateScreen(
+                        (AddVocabularyWordScreen s) => s.WithSpelling(m.Spelling));
+
+                case ModifyPhraseMessage m:
+                    return currentState.UpdateScreen(
+                        (AddVocabularyWordScreen s) => s.WithPhrase(m.Phrase));
+
+                case ModifyPronunciationMessage m:
+                    return currentState.UpdateScreen(
+                        (AddVocabularyWordScreen s) => s.WithPronunciation(m.Pronunciation));
+            }
+
+            return currentState;
+        }
+    }
+}
