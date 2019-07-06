eca168481f333e049e18857b491fe9ff499315a4
Unknown <bearps@microsoft.com>
2019-07-06 15:48:19 +1200

    Create VocabularyBrowserScreenReducer and test

---
+++ src/WordTutor.Core/Reducers/VocabularyBrowserScreenReducer.cs
@@ -0,0 +1,25 @@
+﻿using WordTutor.Core.Actions;
+using WordTutor.Core.Redux;
+
+namespace WordTutor.Core.Reducers
+{
+    public class VocabularyBrowserScreenReducer : IReduxReducer<WordTutorApplication>
+    {
+        public WordTutorApplication Reduce(IReduxMessage message, WordTutorApplication currentState)
+        {
+            if (!(currentState.CurrentScreen is VocabularyBrowserScreen))
+            {
+                return currentState;
+            }
+
+            switch(message)
+            {
+                case SelectWordMessage m:
+                    return currentState.UpdateScreen(
+                        (VocabularyBrowserScreen s) => s.WithSelection(m.Word));
+            }
+
+            return currentState;
+        }
+    }
+}
