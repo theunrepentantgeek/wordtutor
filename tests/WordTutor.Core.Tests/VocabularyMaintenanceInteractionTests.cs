480b9abccf1e756ffc4c9fc09958f9429bc14784
Unknown <bearps@microsoft.com>
2019-07-07 21:49:00 +1200

    Create first test with supporting infrastructure

---
+++ tests/WordTutor.Core.Tests/VocabularyMaintenanceInteractionTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+using static Niche.GherkinSyntax.GherkinFactory;
+
+namespace WordTutor.Core.Tests
+{
+    public class VocabularyMaintenanceInteractionTests : InteractionTestsBase
+    {
+        [Fact]
+        public void WhenStartingNewVocabularyWord_AddVocabularyWordScreenOpens() =>
+            Given(ApplicationStateWithVocabulary, MusicPace)
+            .And(CurrentScreenIs, VocabularyBrowserScreen)
+            .When(TheActionIs, OpenNewWordScreen())
+            .Then(AssertTheCurrentScreenIs<AddVocabularyWordScreen>);
+    }
+}
