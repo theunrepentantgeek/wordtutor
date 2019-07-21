b30fa355a4a7f644cc1e9ecbd70439603aa08537
Unknown <bearps@microsoft.com>
2019-07-21 12:28:26 +1200

    Create WordTutorApplicationStateFactory

--- src/WordTutor.Desktop/Program.cs
+++ src/WordTutor.Desktop/Program.cs
@@ -1,4 +1,4 @@
-﻿using SimpleInjector;
+using SimpleInjector;
 using System;
 using System.Reflection;
 using System.Windows;
@@ -8,16 +8,16 @@ using WordTutor.Core.Reducers;
 using WordTutor.Core.Redux;
 using WordTutor.Desktop;
 
-static class Program
+static partial class Program
 {
     [STAThread]
     static void Main()
     {
-        var application = CreateApplicationModel();
-        var store = CreateStore(application);
-        var container = CreateContainer(store);
+        var container = CreateContainer();
 
         var app = new App();
+
+        var store = container.GetInstance<ReduxStore<WordTutorApplication>>();
 
         var model = new VocabularyBrowserViewModel(store);
         var view = new VocabularyBrowserView
@@ -31,60 +31,19 @@ static class Program
         app.Run(mainWindow);
     }
 
-    private static IReduxStore<WordTutorApplication> CreateStore(WordTutorApplication application)
-    {
-        var reducer = new CompositeReduxReducer<WordTutorApplication>(
-            new IReduxReducer<WordTutorApplication>[]
-            {
-                new AddVocabularyWordScreenReducer(),
-                new VocabularyBrowserScreenReducer()
-            });
-
-        var store = new ReduxStore<WordTutorApplication>(reducer, application);
-        return new LoggingReduxStore<WordTutorApplication>(store);
-    }
-
-    private static WordTutorApplication CreateApplicationModel()
-    {
-        var alpha = new VocabularyWord("alpha")
-            .WithPhrase("The alpha dog")
-            .WithPronunciation("alfa");
-
-        var beta = new VocabularyWord("beta")
-            .WithPhrase("A beta release")
-            .WithPronunciation("beta");
-
-        var gamma = new VocabularyWord("gamma")
-            .WithPhrase("Gamma radiation")
-            .WithPronunciation("gamma");
-
-        var delta = new VocabularyWord("delta")
-            .WithPhrase("Change is often called delta.")
-            .WithPronunciation("delta");
-
-        var vocabulary = VocabularySet.Empty
-            .Add(alpha)
-            .Add(beta)
-            .Add(gamma)
-            .Add(delta);
-
-        var screen = new VocabularyBrowserScreen()
-            .WithSelection(gamma);
-
-        var application = new WordTutorApplication(screen)
-            .WithVocabularySet(vocabulary);
-
-        return application;
-    }
-
     private static Container CreateContainer()
     {
         var container = new Container();
+        var coreAssembly = typeof(WordTutorApplication).Assembly;
+        var desktopAssembly = Assembly.GetExecutingAssembly();
 
         // Register Redux 
         container.RegisterSingleton<
             IReduxStore<WordTutorApplication>,
             ReduxStore<WordTutorApplication>>();
+        container.RegisterSingleton<
+            IReduxStateFactory<WordTutorApplication>,
+            WordTutorApplicationStateFactory>();
 
         container.Verify();
 
