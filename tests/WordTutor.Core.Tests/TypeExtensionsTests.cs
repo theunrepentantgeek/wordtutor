4d5064b2929a321aa1e7ff7d40ad024d527bcf99
Bevan Arps <bevan.arps@outlook.com>
2019-11-12 10:10:38 +1300

    Define IsImmutableType() extension method and test

---
+++ tests/WordTutor.Core.Tests/TypeExtensionsTests.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Immutable;
+using System.Text;
+using Xunit;
+
+namespace WordTutor.Core.Tests
+{
+    public class TypeExtensionsTests
+    {
+        public class IsImmutableType: TypeExtensionsTests
+        {
+            [Theory]
+            [InlineData(typeof(VocabularyWord), true)]
+            [InlineData(typeof(string), true)]
+            [InlineData(typeof(StringBuilder), false)]
+            [InlineData(typeof(IImmutableSet<VocabularyWord>), true)]
+            [InlineData(typeof(IImmutableSet<StringBuilder>), false)]
+            public void GivenType_ReturnsExpectedResult(Type type, bool isImmutable)
+            {
+                var modifier = isImmutable ? "" : "not ";
+                type.IsImmutableType()
+                    .Should().Be(
+                        isImmutable,
+                        $"Type {type.Name} should {modifier} be immutable.");
+            }
+        }
+    }
+}
