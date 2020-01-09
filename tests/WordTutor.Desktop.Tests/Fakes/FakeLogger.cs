4c3a6e1231b3f281d19e7c78e8a4c6f15194d24c
Bevan Arps <bevan.arps@outlook.com>
2020-01-09 21:06:20 +1300

    Add FakeLogger and update tests

---
+++ tests/WordTutor.Desktop.Tests/Fakes/FakeLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WordTutor.Core.Logging;
+
+namespace WordTutor.Desktop.Tests.Fakes
+{
+    public class FakeLogger : ILogger, IActionLogger
+    {
+        public IActionLogger Action(string message)
+            => this;
+
+        public void Debug(string message)
+        {
+        }
+
+        public void Dispose()
+        {
+        }
+
+        public void Failure(string message)
+        {
+        }
+
+        public void Info(string message)
+        {
+        }
+
+        public void Success(string message)
+        {
+        }
+    }
+}
