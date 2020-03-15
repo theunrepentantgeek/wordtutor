8a98574bf8ed24982b44cede4c0981884bbbf997
Bevan Arps <bevan.arps@outlook.com>
2020-03-15 13:09:32 +1300

    Introduce interface IReduxDispatcher

---
+++ src/WordTutor.Core/Redux/IReduxDispatcher.cs
@@ -0,0 +1,11 @@
+﻿namespace WordTutor.Core.Redux
+{
+    public interface IReduxDispatcher
+    {
+        /// <summary>
+        /// Dispatch an application message
+        /// </summary>
+        /// <param name="message">Message to process.</param>
+        void Dispatch(IReduxMessage message);
+    }
+}
