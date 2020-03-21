eaf2f9fe0f5e8b283b7d440e2dd5b23ff31ddb5e
Bevan Arps <bevan.arps@outlook.com>
2020-03-21 17:19:40 +1300

    Rework FakeReducer to immutable form

--- tests/WordTutor.Core.Tests/Fakes/FakeReducer.cs
+++ tests/WordTutor.Core.Tests/Fakes/FakeReducer.cs
@@ -5,9 +5,12 @@ namespace WordTutor.Core.Tests.Fakes
 {
     public sealed class FakeReducer<T> : IReduxReducer<T>
     {
-        public Func<IReduxMessage, T, T> Reduce { get; set; } = (_, s) => s;
+        private Func<IReduxMessage, T, T> _reduce;
+
+        public FakeReducer(Func<IReduxMessage, T, T> reduce)
+            => _reduce = reduce ?? throw new ArgumentNullException(nameof(reduce));
 
         T IReduxReducer<T>.Reduce(IReduxMessage message, T currentState)
-            => Reduce(message, currentState);
+            => _reduce(message, currentState);
     }
 }
