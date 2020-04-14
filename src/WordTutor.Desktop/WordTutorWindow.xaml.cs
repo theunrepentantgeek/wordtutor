3d9921d0d9203ba4ce128dd2dbddf1726e1b50cd
Bevan Arps <bevan.arps@outlook.com>
2020-04-14 21:21:49 +1200

    Remove button from main window

--- src/WordTutor.Desktop/WordTutorWindow.xaml.cs
+++ src/WordTutor.Desktop/WordTutorWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using WordTutor.Core.Services;
 
 namespace WordTutor.Desktop
@@ -21,26 +8,16 @@ namespace WordTutor.Desktop
     /// </summary>
     public partial class WordTutorWindow : Window
     {
-        private readonly ISpeechService speechService;
-
-        public WordTutorWindow(
-            ViewModelToViewValueConverter converter,
-            ISpeechService speechService)
+        public WordTutorWindow(ViewModelToViewValueConverter converter)
         {
             Resources.Add("ViewModelToViewValueConverter", converter);
 
             InitializeComponent();
-            this.speechService = speechService;
         }
 
         private void ButtonExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            speechService.SayAsync("Hello World.");
         }
     }
 }
