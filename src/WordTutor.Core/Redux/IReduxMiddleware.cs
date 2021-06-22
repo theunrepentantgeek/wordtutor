4a931fb8cbdf9114a72ba33a2a86cbe7b332d51b
Bevan Arps <bevan.arps@outlook.com>
2021-06-22 17:09:53 +1200

    CA1716 - Rename parameters

--- src/WordTutor.Core/Redux/IReduxMiddleware.cs
+++ src/WordTutor.Core/Redux/IReduxMiddleware.cs
@@ -6,7 +6,7 @@
         /// Intercept, filter or fork an application message, optionally passing it to the next handler in the chain
         /// </summary>
         /// <param name="message">Message to process.</param>
-        /// <param name="next">Next dispatcher to invoke.</param>
-        void Dispatch(IReduxMessage message, IReduxDispatcher next);
+        /// <param name="nextDispatcher">Next dispatcher to invoke.</param>
+        void Dispatch(IReduxMessage message, IReduxDispatcher nextDispatcher);
     }
 }
